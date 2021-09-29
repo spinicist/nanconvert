a2753823e331f8247c0e6592418cd0cce40c914e
Tobias Wood <tobias@spinicist.org.uk>
2021-09-29 17:45:55 +0100

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,28 @@
-#!/bin/bash -eux
-vcpkg/bootstrap-vcpkg.sh -allowAppleClang -useSystemBinaries
-cmake -B build -S . \
-    -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_TARGET_TRIPLET=x64-osx-asan -DCMAKE_BUILD_TYPE=Release \
-    -GNinja
-cmake --build build
+#!/bin/bash -eu
+
+# This script will bootstrap an initial configure & build.
+# On Linux and Mac a toolchain will be used which includes -march=native and
+# ASan/UBSan support. Other platforms will use the default toolchain.
+
+git submodule update --init --recursive
+
+PLATFORM="$( uname -s )"
+case "$PLATFORM" in
+  Linux*)  TOOLCHAIN="../cmake/x64-linux-native.toolchain.cmake";;
+  Darwin*) TOOLCHAIN="../cmake/x64-osx-native.toolchain.cmake";;
+  *)       TOOLCHAIN="../cmake/vcpkg/scripts/buildsystems/vcpkg.cmake";;
+esac
+
+# Use Ninja if available, otherwise CMake default
+if [ -x "$( command -v ninja )" ]; then
+  GEN="-GNinja"
+else
+  GEN=""
+fi
+
+mkdir -p build
+cd build
+cmake -S ../ $GEN \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN"
+cmake --build .
