eb7e84188d91e3e6e0d60f418c31942c9fff8cd8
Tobias Wood <tobias@spinicist.org.uk>
2022-01-05 19:24:08 +0000

    Fixes reference in range for + formats

--- Source/nanconvert_bruker.cpp
+++ Source/nanconvert_bruker.cpp
@@ -12,84 +12,76 @@
 #include <iomanip>
 
 #include "itkImage.h"
-#include "itkMetaDataObject.h"
 #include "itkImageIOFactory.h"
+#include "itkMetaDataObject.h"
 
 #include "Args.h"
-#include "Util.h"
 #include "IO.h"
+#include "Util.h"
 
 /*
  * Declare args here so things like verbose can be global
  */
-args::ArgumentParser parser("Convert Bruker format to whatever you want\nhttp://github.com/spinicist/nanconvert");
+args::ArgumentParser
+    parser("Convert Bruker format to whatever you want\nhttp://github.com/spinicist/nanconvert");
 
 args::Positional<std::string> input_file(parser, "INPUT", "Input file, must include extension.");
-args::Positional<std::string> output_arg(parser, "OUTPUT", "Output file, must include extension. If the rename flag is used, only the extension is used.");
+args::Positional<std::string> output_arg(
+    parser,
+    "OUTPUT",
+    "Output file, must include extension. If the rename flag is used, only the extension is used.");
 
 args::HelpFlag help(parser, "HELP", "Show this help menu", {'h', "help"});
-args::Flag verbose(parser, "VERBOSE", "Print more information", {'v', "verbose"});
+args::Flag     verbose(parser, "VERBOSE", "Print more information", {'v', "verbose"});
 args::Flag double_precision(parser, "DOUBLE", "Write out double precision files", {'d', "double"});
-args::Flag scale(parser, "SCALE", "Scale image x10 for compatibility with SPM etc.", {'s', "scale"});
-args::ValueFlagList<std::string> rename_args(parser, "RENAME", "Rename using specified header fields (can be multiple).", {'r', "rename"});
-args::ValueFlag<std::string> prefix(parser, "PREFIX", "Add a prefix to output filename.", {'p', "prefix"});
+args::Flag
+    scale(parser, "SCALE", "Scale image x10 for compatibility with SPM etc.", {'s', "scale"});
+args::ValueFlagList<std::string> rename_args(
+    parser, "RENAME", "Rename using specified header fields (can be multiple).", {'r', "rename"});
+args::ValueFlag<std::string>
+    prefix(parser, "PREFIX", "Add a prefix to output filename.", {'p', "prefix"});
 
 /*
  * Helper function to work out the name of the output file
  */
-std::string RenameFromHeader(const itk::MetaDataDictionary &header)
-{
-    bool append_delim = false;
+std::string RenameFromHeader(const itk::MetaDataDictionary &header) {
+    bool        append_delim = false;
     std::string output;
-    for (const auto rename_field : args::get(rename_args))
-    {
+    for (const auto &rename_field : args::get(rename_args)) {
         std::vector<std::vector<std::string>> string_array_array_value;
-        std::vector<std::string> string_array_value;
-        std::vector<double> double_array_value;
-        std::string string_value;
-        double double_value;
-        if (!header.HasKey(rename_field))
-        {
+        std::vector<std::string>              string_array_value;
+        std::vector<double>                   double_array_value;
+        std::string                           string_value;
+        double                                double_value;
+        if (!header.HasKey(rename_field)) {
             if (verbose)
-                std::cerr << "Rename field '" << rename_field << "' not found in header. Ignoring" << std::endl;
+                std::cerr << "Rename field '" << rename_field << "' not found in header. Ignoring"
+                          << std::endl;
             continue;
         }
-        if (append_delim)
-        {
+        if (append_delim) {
             output.append("_");
-        }
-        else
-        {
+        } else {
             append_delim = true;
         }
-        if (ExposeMetaData(header, rename_field, string_array_array_value))
-        {
+        if (ExposeMetaData(header, rename_field, string_array_array_value)) {
             output.append(SanitiseString(string_array_array_value[0][0]));
-        }
-        else if (ExposeMetaData(header, rename_field, string_array_value))
-        {
+        } else if (ExposeMetaData(header, rename_field, string_array_value)) {
             output.append(SanitiseString(string_array_value[0]));
-        }
-        else if (ExposeMetaData(header, rename_field, string_value))
-        {
+        } else if (ExposeMetaData(header, rename_field, string_value)) {
             output.append(SanitiseString(string_value));
-        }
-        else if (ExposeMetaData(header, rename_field, double_array_value))
-        {
+        } else if (ExposeMetaData(header, rename_field, double_array_value)) {
             std::ostringstream formatted;
             formatted << double_array_value;
             output.append(SanitiseString(formatted.str()));
-        }
-        else if (ExposeMetaData(header, rename_field, double_value))
-        {
+        } else if (ExposeMetaData(header, rename_field, double_value)) {
             std::ostringstream formatted;
             formatted << double_value;
             output.append(SanitiseString(formatted.str()));
-        }
-        else
-        {
+        } else {
             if (verbose)
-                std::cerr << "Could not determine type of rename header field, ignoring:" << rename_field << std::endl;
+                std::cerr << "Could not determine type of rename header field, ignoring:"
+                          << rename_field << std::endl;
         }
     }
     // Write output name to stdout so outer script can pick it up for method file
@@ -100,19 +92,15 @@ std::string RenameFromHeader(const itk::MetaDataDictionary &header)
 /*
  * Templated conversion functions to avoid macros
  */
-template <typename T, int D>
-void Convert(const std::string &input, const std::string &output)
-{
+template <typename T, int D> void Convert(const std::string &input, const std::string &output) {
     typedef itk::Image<T, D> TImage;
     if (verbose)
         std::cerr << "Reading image: " << input << std::endl;
     auto image = ReadImage<TImage>(input);
-    if (scale)
-    {
+    if (scale) {
         auto spacing = image->GetSpacing();
-        auto origin = image->GetOrigin();
-        for (int i = 0; i < D; i++)
-        {
+        auto origin  = image->GetOrigin();
+        for (int i = 0; i < D; i++) {
             spacing[i] *= 10;
             origin[i] *= 10;
         }
@@ -127,10 +115,8 @@ void Convert(const std::string &input, const std::string &output)
 }
 
 template <typename T>
-void ConvertFile(const std::string &input, const std::string &output, const int D)
-{
-    switch (D)
-    {
+void ConvertFile(const std::string &input, const std::string &output, const int D) {
+    switch (D) {
     case 2:
         Convert<T, 2>(input, output);
         break;
@@ -145,12 +131,12 @@ void ConvertFile(const std::string &input, const std::string &output, const int 
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     ParseArgs(parser, argc, argv);
 
-    const std::string input = CheckPos(input_file);
-    itk::ImageIOBase::Pointer header = itk::ImageIOFactory::CreateImageIO(input.c_str(), itk::ImageIOFactory::ReadMode);
+    const std::string         input = CheckPos(input_file);
+    itk::ImageIOBase::Pointer header =
+        itk::ImageIOFactory::CreateImageIO(input.c_str(), itk::ImageIOFactory::ReadMode);
     if (!header)
         FAIL("Could not open: " << input);
     if (verbose)
@@ -161,30 +147,24 @@ int main(int argc, char **argv)
 
     /* Deal with renaming */
     std::string output_path = prefix.Get();
-    if (rename_args)
-    {
+    if (rename_args) {
         output_path += RenameFromHeader(dict);
         output_path += GetExt(CheckPos(output_arg));
-    }
-    else
-    {
+    } else {
         output_path += CheckPos(output_arg);
     }
 
     auto dims = header->GetNumberOfDimensions();
-    /* We don't need the pixel type because Bruker 'complex' images are real volumes then imaginary volumes */
+    /* We don't need the pixel type because Bruker 'complex' images are real volumes then imaginary
+     * volumes */
 
-    if (double_precision)
-    {
+    if (double_precision) {
         ConvertFile<double>(input, output_path, dims);
-    }
-    else
-    {
+    } else {
         ConvertFile<float>(input, output_path, dims);
     }
 
-    if (dict.HasKey("PVM_DwEffBval"))
-    {
+    if (dict.HasKey("PVM_DwEffBval")) {
         /* It's a diffusion image, write out the b-values and vectors */
         auto bvals = GetMetaData<std::vector<double>>(dict, "PVM_DwEffBval");
         auto bvecs = GetMetaData<std::vector<double>>(dict, "PVM_DwGradVec");
@@ -192,17 +172,14 @@ int main(int argc, char **argv)
         auto bvec_scale = GetMetaData<std::vector<double>>(dict, "PVM_DwGradAmp")[0] / 100.;
 
         std::ofstream bvals_file(StripExt(output_path) + ".bval");
-        for (const auto &bval : bvals)
-        {
+        for (const auto &bval : bvals) {
             bvals_file << bval << "\t";
         }
 
         std::ofstream bvecs_file(StripExt(output_path) + ".bvec");
         bvecs_file << std::setprecision(std::numeric_limits<double>::digits10 + 1);
-        for (int row = 0; row < 3; ++row)
-        {
-            for (size_t col = 0; col < bvals.size(); ++col)
-            {
+        for (int row = 0; row < 3; ++row) {
+            for (size_t col = 0; col < bvals.size(); ++col) {
                 bvecs_file << bvecs[col * 3 + row] / bvec_scale << "\t";
             }
             bvecs_file << "\n";
